fe588eee7b9c26d4cf600049713fee5ea39ef0d7
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-04-11 22:07:48 +0200

    Add savedInstanceState so the list is kept while rotating the app

--- app/src/main/java/com/example/android/codingchallengeshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/android/codingchallengeshoppinglist/MainActivity.java
@@ -37,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         listItem9 = findViewById(R.id.main_item9);
         listItem10 = findViewById(R.id.main_item10);
 
+        if(savedInstanceState != null){
+            listItem1.setText(savedInstanceState.getString("main_item1"));
+            listItem2.setText(savedInstanceState.getString("main_item2"));
+            listItem3.setText(savedInstanceState.getString("main_item3"));
+            listItem4.setText(savedInstanceState.getString("main_item4"));
+            listItem5.setText(savedInstanceState.getString("main_item5"));
+            listItem6.setText(savedInstanceState.getString("main_item6"));
+            listItem7.setText(savedInstanceState.getString("main_item7"));
+            listItem8.setText(savedInstanceState.getString("main_item8"));
+            listItem9.setText(savedInstanceState.getString("main_item9"));
+            listItem10.setText(savedInstanceState.getString("main_item10"));
+            count = savedInstanceState.getInt("count");
+
+
+
+        }
     }
 
     public void addItemLaunch(View view) {
@@ -55,6 +71,22 @@ public class MainActivity extends AppCompatActivity {
                 addItemToList(reply);
             }
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("main_item1", listItem1.getText().toString());
+        savedInstanceState.putString("main_item2", listItem2.getText().toString());
+        savedInstanceState.putString("main_item3", listItem3.getText().toString());
+        savedInstanceState.putString("main_item4", listItem4.getText().toString());
+        savedInstanceState.putString("main_item5", listItem5.getText().toString());
+        savedInstanceState.putString("main_item6", listItem6.getText().toString());
+        savedInstanceState.putString("main_item7", listItem7.getText().toString());
+        savedInstanceState.putString("main_item8", listItem8.getText().toString());
+        savedInstanceState.putString("main_item9", listItem9.getText().toString());
+        savedInstanceState.putString("main_item10", listItem10.getText().toString());
+        savedInstanceState.putInt("count" , count);
     }
 
     public void addItemToList(String returnItem){
@@ -126,4 +158,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void emptyList(View view) {
+        listItem1.setText("");
+        listItem2.setText("");
+        listItem3.setText("");
+        listItem4.setText("");
+        listItem5.setText("");
+        listItem6.setText("");
+        listItem7.setText("");
+        listItem8.setText("");
+        listItem9.setText("");
+        listItem10.setText("");
+        count = 1;
+    }
 }
